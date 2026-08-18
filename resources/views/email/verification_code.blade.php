1f7aa87ddd088c8b37691716a716cdad61a9f157
abdullahshah4650-debug <abdullahshah4650@gmail.com>
2026-08-19 01:52:45 +0500

    Added Two Factor Authentication

---
+++ resources/views/email/verification_code.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Hello
+    </p>
+    <p> Your Login Verification Code is : <strong>{{ $code }}</strong></p>
+    <p>Please Enter This Code To Login And Don't Share This Code To Anyone</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,9 +12,25 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
+Route::post('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+Route::get('/verify', [AdminController::class, 'ShowVerification'])->name('custom.verification.form');
+
+Route::post('/verify', [AdminController::class, 'VerificationVerify'])->name('custom.verification.verify');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+});
